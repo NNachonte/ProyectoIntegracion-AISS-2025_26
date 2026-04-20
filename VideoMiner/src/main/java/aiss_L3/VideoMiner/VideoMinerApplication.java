6c74d960ad91bfb24615b6f58877db49ddbb08ce
NNachonte <igngarcialopez1902@gmail.com>
2026-04-20 13:46:23 +0200

    Add files via upload

---
+++ VideoMiner/src/main/java/aiss_L3/VideoMiner/VideoMinerApplication.java
@@ -0,0 +1,13 @@
+package aiss_L3.VideoMiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoMinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoMinerApplication.class, args);
+	}
+
+}
---
+++ VideoMiner/src/test/java/aiss_L3/VideoMiner/VideoMinerApplicationTests.java
@@ -0,0 +1,13 @@
+package aiss_L3.VideoMiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoMinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
